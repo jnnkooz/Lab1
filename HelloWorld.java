3d40da257737de8e78df545b61169d75b639e79e
Dylan saly <jnnkooz@gmail.com>
2025-02-07 19:20:31 -0800

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * This is my first java app. it prints out 
+ * Hello World to the console
+ * @author Dylan saly
+ * @version v1.0
+ * @since 2/6/2025
+ */
+public class HelloWorld {
+public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("Dylan saly");
+    
+}
+}
